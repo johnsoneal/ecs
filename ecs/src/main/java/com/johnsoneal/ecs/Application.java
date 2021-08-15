10faffd6f46458453dde0004b0cb9f7f3d823c3c
neal <johnsoneal@gmail.com>
2021-08-15 16:41:05 +0100

    Removed swagger-ui as it fails?

--- ecs/src/main/java/com/johnsoneal/ecs/Application.java
+++ ecs/src/main/java/com/johnsoneal/ecs/Application.java
@@ -8,30 +8,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
 @EnableJpaRepositories("com.johnsoneal.ecs.cars")
 @EntityScan("com.johnsoneal.ecs.cars")
-@EnableSwagger2
 @Configuration
 public class Application
 {
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public Docket documentation()
-    {
-        return new Docket(DocumentationType.OAS_30).select()
-            .apis(RequestHandlerSelectors.basePackage(
-                "com.johnsoneal.ecs"))
-            .build();
     }
 
     @Bean
