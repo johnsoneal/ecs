20302845b79aa7ae93c13af74e858655883132ab
neal <johnsoneal@gmail.com>
2021-08-15 13:29:25 +0100

    Basic setup for application from SpringBoot Starter.

---
+++ ecs/src/main/java/com/johnsoneal/ecs/Application.java
@@ -0,0 +1,42 @@
+package com.johnsoneal.ecs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableJpaRepositories("com.johnsoneal.ecs.car")
+@EntityScan("com.johnsoneal.ecs.car")
+@EnableSwagger2
+@Configuration
+public class Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket documentation()
+    {
+        return new Docket(DocumentationType.OAS_30).select()
+            .apis(RequestHandlerSelectors.basePackage(
+                "com.johnsoneal.ecs"))
+            .build();
+    }
+
+    @Bean
+    public ModelMapper modelMapper()
+    {
+        return new ModelMapper();
+    }
+}
---
+++ ecs/src/test/java/com/johnsoneal/ecs/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.johnsoneal.ecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests
+{
+    @Test
+    void contextLoads()
+    {
+        // Just test that we have everything configured correctly.
+    }
+}
